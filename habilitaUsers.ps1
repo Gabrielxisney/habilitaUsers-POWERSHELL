859ac3f3ce7eed11e91b6d3e6c45992e9f7d6f4a
Gabriel Alexandre Rosa <102827318+Gabrielxisney@users.noreply.github.com>
2024-01-10 10:25:31 -0300

    Add files via upload

---
+++ habilitaUsers.ps1
@@ -0,0 +1,9 @@
+﻿$caminhoArquivo = "C:\Users\garosa\Desktop\habilitarUsers.txt"
+
+$users = Get-Content -Path $caminhoArquivo
+
+echo $users
+
+#Enable-ADAccount -Identity $users
+#Set-ADUser -UserPrincipalName agasilva@novaquest.com.br
+#Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText"AGA@2024" -Force)
